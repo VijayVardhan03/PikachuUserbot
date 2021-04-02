eae39a0b47a713d005e7b88effa89d7eb2fedaaa
Sachin Jangir <support@ItzSjDude.in>
2021-04-02 23:52:45 +0530

    Update __init__.py

--- pikabot/core/utilities/__init__.py
+++ pikabot/core/utilities/__init__.py
@@ -1,1 +1,18 @@
 
+from telethon import TelegramClient
+from telethon.sessions import StringSession
+def PikaClient(session, gBot=None):
+    if gBot:
+        return TelegramClient(
+            "TgBot",
+            pdb.Api_id,
+            pdb.Api_hash).start(
+            bot_token=session)
+    else:
+        return TelegramClient(
+            StringSession(session),
+            pdb.Api_id,
+            pdb.Api_hash,
+            connection_retries=None,
+            auto_reconnect=True,
+            lang_code='en')
