bd1645c935f5144e8a38eba120388c263e278783
Sachin Jangir <support@ItzSjDude.in>
2021-04-03 17:05:21 +0530

    Update __init__.py

--- pikabot/loader/__init__.py
+++ pikabot/loader/__init__.py
@@ -36,7 +36,7 @@ def pika_plugins(_pikamod=None):
     spec = spec_from_file_location(name, path)
     _pika = module_from_spec(spec)
                                    #____Pikabot__Plugins__Loader____
-    userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.command = Pikapi; _pika.ItzSjDude = ItzSjDude; _pika.pikaa = pikaa; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
+    userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.ItzSjDude = ItzSjDude; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
     modules["SysRuntime"] = pikabot.main_plugs.SysRuntime; modules["userbot"] = pikabot; modules["userbot.utils"] = _utilz; spec.loader.exec_module(_pika); bot.pika_cmd[_pikamod] = _pika; modules["pikabot"+_pikamod] = _pika; logpl.info("🔥Imported "+_pikamod)
 
 def load_ext_module(shortname):
