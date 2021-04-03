0f73f3c29ccdbe99fe45147c37860283e179e66f
Sachin Jangir <support@ItzSjDude.in>
2021-04-03 19:43:19 +0530

    Update decorators.py

--- pikabot/utils/_client/decorators.py
+++ pikabot/utils/_client/decorators.py
@@ -260,6 +260,51 @@ def ItzSjDude(**args):
         return wrapper
     return decorator
 
+def admin_cmd(pattern=None, **args):
+    args["func"] = lambda e: e.via_bot_id is None
+
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    allow_sudo = args.get("allow_sudo", False)
+
+    # get the pattern from the decorator
+    if pattern is not None:
+        if pattern.startswith("\#"):
+            # special fix for snip.py
+            args["pattern"] = re.compile(pattern)
+        else:
+            args["pattern"] = re.compile("\." + pattern)
+            cmd = "." + pattern
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except:
+                CMD_LIST.update({file_test: [cmd]})
+
+    args["outgoing"] = True
+    # should this command be available for other users?
+    if allow_sudo:
+        args["from_users"] = list(Var.SUDO_USERS)
+        # Mutually exclusive with outgoing (can only set one of either).
+        args["incoming"] = True
+        del args["allow_sudo"]
+
+    # error handling condition check
+    elif "incoming" in args and not args["incoming"]:
+        args["outgoing"] = True
+
+    # add blacklist chats, UB should not respond in these chats
+    allow_edited_updates = False
+    if "allow_edited_updates" in args and args["allow_edited_updates"]:
+        allow_edited_updates = args["allow_edited_updates"]
+        del args["allow_edited_updates"]
+
+    # check if the plugin should listen for outgoing 'messages'
+    is_message_enabled = True
+
+    return events.NewMessage(**args)
+
 async def progress(current, total, event, start, type_of_ps, file_name=None):
     """Generic progress_callback for both
     upload.py and download.py"""
@@ -350,4 +395,4 @@ class Loader():
         bot.add_event_handler(func, events.NewMessage(**args))
 
 
-__all__=['ItzSjDude', 'pika_sudo', 'time_formatter', 'get_readable_time', 'humanbytes', 'progress']
+__all__=['ItzSjDude', 'pika_sudo', 'admin_cmd', 'time_formatter', 'get_readable_time', 'humanbytes', 'progress']
