fcdf29f13eca023c83ca7ca9899abf0c5a03679b
Sachin Jangir <support@ItzSjDude.in>
2021-04-03 07:43:13 +0530

    Update load_module.py

--- pikabot/utils/client/load_module.py
+++ pikabot/utils/client/load_module.py
@@ -2,35 +2,33 @@ from telethon import *
 from importlib.util import *
 from ....resources.reqfxns import *
 import logging
+import sys           
+from sys import modules
+from ..import utils 
+from pathlib import Path as _asstpath
+import plugins.__init__ as _Modules
 
+def pika_assistant(_pikasst=None):
+    if ACTIVATE_ASSISTANT:
+       asstpath = _asstpath(f"./pikabot/Assistant/plugins/{_pikasst}.py")
+       asstname = "pikabot.Assistant.plugins.{}".format(_pikasst)
+       spec = spec_from_file_location(asstname, asstpath)
+       asst = module_from_spec(spec)
+                                   #____Pikabot__Assistant__Plugins__Loader____
+       userbot = pikabot; asst.bot = bot; asst.tgbot = tgbot; asst.Var = Var; asst.rx = rx; asst.ItzSjDude = ItzSjDude; asst.pikatgbot = pikatgbot; modules['Asst_modules'] = _Modules       
+       PikaAsst[_pikasst] = asst; modules["pikabot"+_pikasst] = asst; tgbot.PikaAsst[_pikasst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_pikasst)
+       
+    else: 
+       return 
 
-def load_all_modules(shortname):
-    if shortname.startswith("__"):
-        pass
-    else:
-        path = Path(f"plugins/{shortname}.py")
-        name = "plugins.{}".format(shortname)
-        spec = spec_from_file_location(name, path)
-        mod = module_from_spec(spec)
-        mod.bot = bot
-        mod.Var = Var
-        mod.rx = rx
-        mod.command = ItzSjDude
-        mod.ItzSjDude = ItzSjDude
-        mod.logger = logging.getLogger(shortname)
-        sys.modules["SysRuntime"] =
-        sys.modules["userbot"] = pikabot
-        sys.modules["userbot.utils"] = pikabot.utils
-        mod.Config = Var
-        mod.borg = bot
-        sys.modules["uniborg.util"] = pikabot.utils
-        # support for paperplaneextended
-        spec.loader.exec_module(mod)
-        bot.pika_cmd[shortname] = mod
-        # for imports
-        sys.modules["pikabot" + shortname] = mod
-        logpl.info("Successfully (re)imported " + shortname)
-
+def pika_plugins(_pikamod=None):
+    path = Path(f"plugins/{_pikamod}.py")
+    name = "plugins.{}".format(_pikamod)
+    spec = spec_from_file_location(name, path)
+    _pika = module_from_spec(spec)
+                                   #____Pikabot__Plugins__Loader____
+    userbot = pikabot; _pika.bot = bot; _pika.Var = Var; _pika.rx = rx; _pika.command = Pikapi; _pika.ItzSjDude = ItzSjDude; _pika.pikaa = pikaa; _pika.pika_start = pikarestart; _pika.Config = Var; _pika.borg = bot; _pika.logger = logging.getLogger(_pikamod)
+    modules["SysRuntime"] = pikabot.main_plugs.SysRuntime; modules["userbot"] = pikabot; modules["userbot.utils"] = pikabot.utils; modules["uniborg.util"] = pikabot.utils; spec.loader.exec_module(_pika); bot.pika_cmd[_pikamod] = _pika; modules["pikabot"+_pikamod] = _pika; logpl.info("🔥Imported "+_pikamod)
 
 def load_ext_module(shortname):
     if shortname.endswith("_"):
