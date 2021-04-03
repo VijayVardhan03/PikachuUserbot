636e554c4633b082cac876e75294942ce1c0cc52
Sachin Jangir <support@ItzSjDude.in>
2021-04-03 13:02:41 +0530

    Update __main__.py

--- pikabot/__main__.py
+++ pikabot/__main__.py
@@ -165,6 +165,7 @@ async def init_pika():
             if bot4:
                 await client4.run_until_disconnected()
 
-
+  
 if __name__ == "__main__":
-    
+    pikaloop = asyncio.get_event_loop()
+    pikaloop.run_until_complete(init_pika())
