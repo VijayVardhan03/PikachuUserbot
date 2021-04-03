1f680f58d0303cd12b312e6f27d9bc15d88bad2a
Sachin Jangir <support@ItzSjDude.in>
2021-04-03 15:35:06 +0530

    Update __init__.py

--- pikabot/clients/__init__.py
+++ pikabot/clients/__init__.py
@@ -1,27 +1,21 @@
 from ..core import pdb, pget, PikaClient
 
-Alpha = pget("alpha", "session")
-Beta = pget("beta", "session")
-Gaama = pget("gaama", "session")
-Delta = pget("delta", "session")
-Asstt = pget("omega", "assistant")
-
-if Alpha:
+if pdb.Alpha:
     bot = client = PikaClient(Alpha)
 else:
     bot = client = None
 
-if Beta:
+if pdb.Beta:
     bot2 = client2 = PikaClient(Beta)
 else:
     bot2 = client2 = None
 
-if Gaama:
+if pdb.Gaama:
     bot3 = client3 = PikaClient(Gaama)
 else:
     bot3 = None
 
-if Delta:
+if pdb.Delta:
     bot4 = client4 = PikaClient(Delta)
 else:
     bot4 = client4 = None
