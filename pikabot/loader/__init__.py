c78e092e6a84ed3f7ea42991b48410af40546813
Sachin Jangir <support@ItzSjDude.in>
2021-04-12 07:14:09 +0530

    Update __init__.py

--- pikabot/loader/__init__.py
+++ pikabot/loader/__init__.py
@@ -16,15 +16,15 @@ logpa = getLogger("Assistant:")
 
 def pika_assistant(_pikasst=None):
     if pdb.Asstt:
-       rx = "!"
-       import plugins.__init__ as _Modules
-       asstpath = _asstpath(f"./pikabot/Assistant/plugins/{_pikasst}.py")
-       asstname = "pikabot.Assistant.plugins.{}".format(_pikasst)
-       spec = spec_from_file_location(asstname, asstpath)
-       asst = module_from_spec(spec)
-                                   #____Pikabot__Assistant__Plugins__Loader____
-       userbot = pikabot; asst.bot = bot; asst.tgbot = tgbot; asst.Var = Var; asst.rx = rx; asst.ItzSjDude = ItzSjDude; asst.pikatgbot = pikatgbot; modules['Asst_modules'] = _Modules       
-       PikaAsst[_pikasst] = asst; modules["pikabot"+_pikasst] = asst; tgbot.PikaAsst[_pikasst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_pikasst)
+    rx = "!"
+    import plugins.__init__ as _Modules
+    asstpath = _asstpath(f"./pikabot/Assistant/plugins/{_pikasst}.py")
+    asstname = "pikabot.Assistant.plugins.{}".format(_pikasst)
+    spec = spec_from_file_location(asstname, asstpath)
+    asst = module_from_spec(spec)
+                               #____Pikabot__Assistant__Plugins__Loader____
+    userbot = pikabot; asst.bot = bot; asst.tgbot = tgbot; asst.Var = Var; asst.rx = rx; asst.ItzSjDude = ItzSjDude; asst.pikatgbot = pikatgbot; modules['Asst_modules'] = _Modules       
+    PikaAsst[_pikasst] = asst; modules["pikabot"+_pikasst] = asst; tgbot.PikaAsst[_pikasst] = asst; spec.loader.exec_module(asst); logpa.info("🔥Imported "+_pikasst)
        
     else: 
        return 
