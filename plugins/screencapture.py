5f006315bab74f221ff23f20c70f7599dbeb1a79
Sachin Jangir <support@ItzSjDude.in>
2021-04-12 11:32:22 +0530

    Update screencapture.py

--- plugins/screencapture.py
+++ plugins/screencapture.py
@@ -11,19 +11,15 @@ from pikabot.utils import ItzSjDude
 async def _(event):
     if event.fwd_from:
         return
-    if Config.SCREEN_SHOT_LAYER_ACCESS_KEY is None:
-        await event.edit(
+    a = await pika_msg(event, "**Processing...**")
+    if pdb.ssapi is None:
+        await pika_msg(a,
             "Need to get an API key from https://screenshotlayer.com/product \nModule stopping!"
         )
         return
-    await event.edit("Processing ...")
     sample_url = "https://api.screenshotlayer.com/api/capture?access_key={}&url={}&fullpage={}&viewport={}&format={}&force={}"
     input_str = event.pattern_match.group(1)
-    response_api = requests.get(
-        sample_url.format(
-            Config.SCREEN_SHOT_LAYER_ACCESS_KEY, input_str, "1", "2560x1440", "PNG", "1"
-        )
-    )
+    response_api = requests.get(sample_url.format(pdb.ssapi, input_str, "1", "2560x1440", "PNG", "1"))
     # https://stackoverflow.com/a/23718458/4723940
     contentType = response_api.headers["content-type"]
     if "image" in contentType:
@@ -37,8 +33,8 @@ async def _(event):
                     force_document=True,
                     reply_to=event.message.reply_to_msg_id,
                 )
-                await event.delete()
+                await a.delete()
             except Exception as e:
-                await event.edit(str(e))
+                await pika_msg(a, str(e))
     else:
-        await event.edit(response_api.text)
+        await pika_msg(a, response_api.text)
