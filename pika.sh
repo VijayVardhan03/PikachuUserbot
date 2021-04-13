52a4834773fc5872ff4c33aa1cb58f440dd50d2d
Sachin Jangir <support@ItzSjDude.in>
2021-04-13 16:12:21 +0530

    Update pika.sh

--- pika.sh
+++ pika.sh
@@ -23,7 +23,7 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ=Asia/Kolkata
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-apt-get update && apt upgrade -y && add-apt-repository ppa:deadsnakes/ppa && apt-get install -y --no-install-recommends \
+apt-get update && apt-get install software-properties-common && add-apt-repository ppa:deadsnakes/ppa && apt-get install -y --no-install-recommends \
     coreutils \
     gifsicle \
     apt-utils \
